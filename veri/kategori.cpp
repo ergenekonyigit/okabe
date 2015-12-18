ebb9e549c669ce73970785942e93519b55851c02
ergenekonyigit <ergenekonyigit@hotmail.com>
2015-12-18 19:38:17 +0200

    get-set fonksiyonlari eklendi

--- veri/kategori.cpp
+++ veri/kategori.cpp
@@ -1,5 +1,25 @@
 #include "kategori.h"
 
+
+int Kategori::getKategoriKodu()
+{
+  return kategoriKodu;
+}
+
+void Kategori::setKategoriKodu(int value)
+{
+  kategoriKodu = value;
+}
+
+string Kategori::getKategoriAdi()
+{
+  return kategoriAdi;
+}
+
+void Kategori::setKategoriAdi(string value)
+{
+  kategoriAdi = value;
+}
 Kategori::Kategori()
 {
 
--- veri/urun.cpp
+++ veri/urun.cpp
@@ -1,5 +1,25 @@
 #include "urun.h"
 
+
+int Urun::getUrunKodu()
+{
+  return urunKodu;
+}
+
+void Urun::setUrunKodu(int value)
+{
+  urunKodu = value;
+}
+
+string Urun::getUrunAdi()
+{
+  return urunAdi;
+}
+
+void Urun::setUrunAdi(string value)
+{
+  urunAdi = value;
+}
 Urun::Urun()
 {
 
