abd3458e1966dbe966f810d3a6f9a9a1547a769f
lacriment <lacriment@gmail.com>
2015-12-18 19:39:37 +0200

    Satis modeli güncellendi

--- veri/satis.cpp
+++ veri/satis.cpp
@@ -1,5 +1,55 @@
 #include "satis.h"
 
+int Satis::getSatisKodu()
+{
+    return satisKodu;
+}
+
+void Satis::setSatisKodu(int value)
+{
+    satisKodu = value;
+}
+
+int Satis::getUrunKodu()
+{
+    return urunKodu;
+}
+
+void Satis::setUrunKodu(int value)
+{
+  urunKodu = value;
+}
+
+string Satis::getSatisTarihi()
+{
+  return satisTarihi;
+}
+
+void Satis::setSatisTarihi(string value)
+{
+  satisTarihi = value;
+}
+
+float Satis::getKDV()
+{
+  return KDV;
+}
+
+void Satis::setKDV(float value)
+{
+  KDV = value;
+}
+
+int Satis::getSatisAdeti()
+{
+  return satisAdeti;
+}
+
+void Satis::setSatisAdeti(int value)
+{
+  satisAdeti = value;
+}
+
 Satis::Satis()
 {
 
--- veri/satis.h
+++ veri/satis.h
@@ -1,11 +1,31 @@
 #ifndef SATIS_H
 #define SATIS_H
 
+#include <string>
+
+using namespace std;
 
 class Satis
 {
+private:
+  int satisKodu;
+  int urunKodu;
+  string satisTarihi;
+  float satisFiyati;
+  float KDV;
+  int satisAdeti;
 public:
   Satis();
+  int getSatisKodu();
+  void setSatisKodu(int value);
+  int getUrunKodu();
+  void setUrunKodu(int value);
+  string getSatisTarihi();
+  void setSatisTarihi(string value);
+  float getKDV();
+  void setKDV(float value);
+  int getSatisAdeti();
+  void setSatisAdeti(int value);
 };
 
 #endif // SATIS_H
