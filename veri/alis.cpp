3188cfc9834ad4c93a488d710edf5326b0969f20
lacriment <lacriment@gmail.com>
2015-12-18 19:47:36 +0200

    Alis modeli güncellendi

--- veri/alis.cpp
+++ veri/alis.cpp
@@ -1,7 +1,56 @@
 #include "alis.h"
 
+int Alis::getAlisKodu()
+{
+  return alisKodu;
+}
+
+void Alis::setAlisKodu(int value)
+{
+  alisKodu = value;
+}
+
+int Alis::getUrunKodu()
+{
+  return urunKodu;
+}
+
+void Alis::setUrunKodu(int value)
+{
+  urunKodu = value;
+}
+
+string Alis::getAlisTarihi()
+{
+  return alisTarihi;
+}
+
+void Alis::setAlisTarihi(string value)
+{
+  alisTarihi = value;
+}
+
+float Alis::getAlisFiyati()
+{
+  return alisFiyati;
+}
+
+void Alis::setAlisFiyati(float value)
+{
+  alisFiyati = value;
+}
+
+int Alis::getAlisAdeti()
+{
+  return alisAdeti;
+}
+
+void Alis::setAlisAdeti(int value)
+{
+  alisAdeti = value;
+}
+
 Alis::Alis()
 {
 
 }
-
--- veri/alis.h
+++ veri/alis.h
@@ -1,11 +1,30 @@
 #ifndef ALIS_H
 #define ALIS_H
 
+#include <string>
+
+using namespace std;
 
 class Alis
 {
+private:
+  int alisKodu;
+  int urunKodu;
+  string alisTarihi;
+  float alisFiyati;
+  int alisAdeti;
 public:
   Alis();
+  int getAlisKodu();
+  void setAlisKodu(int value);
+  int getUrunKodu();
+  void setUrunKodu(int value);
+  string getAlisTarihi();
+  void setAlisTarihi(string value);
+  float getAlisFiyati();
+  void setAlisFiyati(float value);
+  int getAlisAdeti();
+  void setAlisAdeti(int value);
 };
 
 #endif // ALIS_H
